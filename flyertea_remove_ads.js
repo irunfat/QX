66e20a25ee08f905700d0fe0203a1107af9cd771
Owen <lzh@live.com>
2021-05-04 16:03:49 +0800

    add flyertea

---
+++ flyertea_remove_ads.js
@@ -0,0 +1,19 @@
+let body = $response.body;
+body=JSON.parse(body);
+
+
+let ad_url_regex = /^https?:\/\/www\.flyert(ea)?\.com\/source\/plugin\/mobile\/mobile\.php\?module=advis/;
+
+
+
+
+if(ad_url_regex.test($request.url)){
+      if (body['Variables'].hasOwnProperty('data')){
+      if (body['Variables']['data'].hasOwnProperty('data')){
+         body['Variables']['data']['data'] = [];
+      }
+    }
+}
+
+body=JSON.stringify(body);
+$done({body});
